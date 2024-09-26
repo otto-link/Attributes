4730fda1042afa83b7700eec950053ab36d977c9
Otto Link <otto.link.bv@gmail.com>
2024-09-26 22:59:35 +0200

    Add doxygen comment

--- Attributes/include/attributes/widgets/abstract_widget.hpp
+++ Attributes/include/attributes/widgets/abstract_widget.hpp
@@ -1,13 +1,21 @@
-/* Copyright (c) 2024 Otto Link. Distributed under the terms of the GNU General
- * Public License. The full license is in the file LICENSE, distributed with
- * this software. */
-
 /**
  * @file abstract_widget.hpp
- * @author Otto Link (otto.link.bv@gmail.com)
- * @brief
+ * @author
+ * Otto Link (otto.link.bv@gmail.com)
  *
- * @copyright Copyright (c) 2024
+ * @brief
+ * This file defines the AbstractWidget class, which serves as a base class
+ * for creating custom Qt widgets that emit a signal when their value changes.
+ *
+ * The AbstractWidget class inherits from QWidget and provides a default
+ * constructor. The signal `value_changed()` is emitted when the widget's value
+ * is modified, making it useful for integrating with signal-slot mechanisms
+ * in Qt-based applications.
+ *
+ * @copyright
+ * Copyright (c) 2024 Otto Link. Distributed under the terms of the GNU General
+ * Public License. The full license is in the file LICENSE, distributed with
+ * this software.
  *
  */
 
@@ -17,14 +25,36 @@
 namespace attr
 {
 
+/**
+ * @class AbstractWidget
+ * @brief A base class for custom Qt widgets that emit a value change signal.
+ *
+ * AbstractWidget is a simple QWidget-based class that emits the `value_changed()`
+ * signal whenever the value within the widget changes. It can be extended by
+ * other widgets to add custom behavior, while maintaining the signal mechanism
+ * for external communication within a Qt application.
+ *
+ * @note The class is part of the attr namespace.
+ */
 class AbstractWidget : public QWidget
 {
   Q_OBJECT
 
 public:
+  /**
+   * @brief Default constructor for AbstractWidget.
+   *
+   * Creates an AbstractWidget instance with default settings.
+   */
   AbstractWidget() = default;
 
 Q_SIGNALS:
+  /**
+   * @brief Signal emitted when the widget's value changes.
+   *
+   * This signal can be connected to other objects in a Qt application to
+   * respond when the widget's internal value changes.
+   */
   void value_changed();
 };
 
