b5f89603978e638050cdc3cf66a800bcdb83b23a
Otto Link <otto.link.bv@gmail.com>
2025-11-01 21:08:57 +0100

    Update QSliderX, use Point2DSelector for Vec2float attr

--- Attributes/include/attributes/widgets/vec2float_widget.hpp
+++ Attributes/include/attributes/widgets/vec2float_widget.hpp
@@ -7,46 +7,13 @@
 #include <QMouseEvent>
 #include <QWidget>
 
+#include "qsx/point2d_selector.hpp"
+
 #include "attributes/vec2float_attribute.hpp"
 #include "attributes/widgets/abstract_widget.hpp"
 
 namespace attr
 {
-
-class XYWidget : public QWidget
-{
-  Q_OBJECT
-
-public:
-  XYWidget() = delete;
-  XYWidget(Vec2FloatAttribute *p_attr);
-
-  void set_point(float x, float y);
-
-signals:
-  void value_changed();
-
-public slots:
-  void center_point();
-  void random_point();
-
-protected:
-  void paintEvent(QPaintEvent *event) override;
-  void mousePressEvent(QMouseEvent *event) override;
-  void mouseMoveEvent(QMouseEvent *event) override;
-  void mouseReleaseEvent(QMouseEvent *event) override;
-
-private:
-  void    handle_mouse_event(const QPoint &widget_point);
-  QPointF map_to_value(const QPointF &widget_point);
-  QPointF map_to_widget(const QPointF &value_point);
-  void    update_attribute_from_widget();
-
-  Vec2FloatAttribute *p_attr;
-  float               radius = 5.f;
-  float               margin = 20.f;
-  QPointF             point;
-};
 
 // =====================================
 // Vec2FloatWidget
@@ -61,8 +28,12 @@ public:
   void reset_value(bool reset_to_initial_state = false) override;
 
 private:
-  Vec2FloatAttribute *p_attr;
-  XYWidget           *xy_widget;
+  void on_center();
+  void on_randomize();
+  void update_attribute_from_widget();
+
+  Vec2FloatAttribute   *p_attr;
+  qsx::Point2DSelector *point2d_selector;
 };
 
 } // namespace attr
